9ffa12a2f96c5cdf4098f4f402b268c9d62f588b
Noah Gibson <noah.gibson99@gmail.com>
2019-10-21 16:04:32 -0600

    Test

--- src/index.js
+++ src/index.js
@@ -25,3 +25,4 @@ ReactDOM.render(router, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
