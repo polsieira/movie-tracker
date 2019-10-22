a340f90c9e7dbbd63e3b4a086ed9b13692f1733f
posi7790 <posi7790@colorado.edu>
2019-10-21 20:24:47 -0600

    Style movie cards and container

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,11 +1,17 @@
 import './MovieCard.scss';
 import React from 'react';
 
-const MovieCard = ({ title, release_date, poster_path }) => {
+const MovieCard = ({ title, release_date, poster_path, backdrop_path }) => {
+  const d = new Date(`${release_date}`);
+  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  const date = `${months[d.getMonth()]} ${d.getDate()}, ${d.getFullYear()}`
   return (
     <div className='MovieCard'>
       <img className='movie-poster' src={`https://image.tmdb.org/t/p/original/${poster_path}`} alt='movie poster' />
-      <h2 className='movie-title'>{title}</h2>
+      <div className='movie-info'>
+        <h2 className='movie-title'>{title}</h2>
+        <h4 className='movie-release'>{date}</h4>
+      </div>
     </div>
   )
 }
