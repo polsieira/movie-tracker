c5dbe60d9eaf01b830ee48802e8ba9f6663b599b
posi7790 <posi7790@colorado.edu>
2019-10-21 19:05:51 -0600

    Display information for new movies

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,10 +1,11 @@
 import './MovieCard.scss';
 import React from 'react';
 
-const MovieCard = ({ }) => {
+const MovieCard = ({ title, release_date, poster_path }) => {
   return (
     <div className='MovieCard'>
-
+      <img className='movie-poster' src={`https://image.tmdb.org/t/p/original/${poster_path}`} alt='movie poster' />
+      <h2 className='movie-title'>{title}</h2>
     </div>
   )
 }
