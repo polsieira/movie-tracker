ed8d37d80ebef18dbb73b64448b08ee5c61c42e5
posi7790 <posi7790@colorado.edu>
2019-10-24 07:49:33 -0600

    Position favoite button in bottom right corner

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -11,9 +11,11 @@ const MovieCard = ({ id, title, release_date, poster_path, backdrop_path }) => {
   return (
     <div className='MovieCard'>
       <img className='movie-poster' src={`https://image.tmdb.org/t/p/original/${poster_path}`} alt='movie poster' />
-      <div className='movie-info'>
-        <h2 className='movie-title'>{title}</h2>
-        <h4 className='movie-release'>{date}</h4>
+      <div className='movie'>
+        <div className='movie-info'>
+          <h2 className='movie-title'>{title}</h2>
+          <h4 className='movie-release'>{date}</h4>
+        </div>
         <button id={id} type='button' className='favorite-btn'><IoIosHeartEmpty className='favorite-heart' /></button>
       </div>
     </div>
