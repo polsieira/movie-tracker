35f861a74b20eb9a91aa528fde8bed463db4750a
posi7790 <posi7790@colorado.edu>
2019-10-28 09:31:40 -0600

    Fix movie card styling

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -10,18 +10,18 @@ const MovieCard = ({ id, title, release_date, poster_path, overview }) => {
   const date = `${months[d.getMonth()]} ${d.getDate()}, ${d.getFullYear()}`;
   return (
     <div className='MovieCard'>
-      <Link to={`/movie/${id}`}>
-      <img className='movie-poster'src={`https://image.tmdb.org/t/p/original/${poster_path}`} alt='movie poster' />
-      <div className='movie'>
-        <div className='movie-info'>
-          <h2 className='movie-title'>{title}</h2>
-        
-          <h4 className='movie-release'>{date}</h4>
-          <h4 className='movie-overview'>{overview}</h4>
+      <Link to={`/movie/${id}`} className='MovieCardLink' >
+        <img className='movie-poster' src={`https://image.tmdb.org/t/p/original/${poster_path}`} alt='movie poster' />
+        <div className='movie'>
+          <div className='movie-info'>
+            <h2 className='movie-title'>{title}</h2>
+
+            <h4 className='movie-release'>{date}</h4>
+            <h4 className='movie-overview'>{overview}</h4>
+          </div>
+          <button id={id} type='button' className='favorite-btn'><IoIosHeartEmpty className='favorite-heart' /></button>
         </div>
-        <button id={id} type='button' className='favorite-btn'><IoIosHeartEmpty className='favorite-heart' /></button>
-      </div>
-    </Link>
+      </Link>
     </div>
   )
 }
